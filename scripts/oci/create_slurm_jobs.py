647a0868bb4fb8eca0526de7a4658fdefc81c40e
Amit Agarwal <amit.h.agarwal@oracle.com>
2025-03-25 16:31:50 -0700

    create batch jobs

--- scripts/oci/create_slurm_jobs.py
+++ scripts/oci/create_slurm_jobs.py
@@ -30,16 +30,17 @@ def create_slurm_job_file(
     results_folder: Path,
     slurm_prefix: str,
     slurm_jobs_folder: Path,
-    batch_size: int
+    batch_size: int,
+    device: str
 ) -> Path:
     """Create slurm job file for running a model on a task"""
     slurm_job = f"{slurm_prefix}\n"
     # slurm_job += f"mteb run -m {model_name} -t {task_name} --output_folder {results_folder.resolve()} --co2_tracker true"
-    slurm_job += f"python benchmark_task.py --model_name  {model_name} --task_name  {task_name} --batch_size {batch_size}"
+    slurm_job += f"python benchmark_task.py --model_name  {model_name} --task_name  {task_name} --batch_size {batch_size} --device {device}"
 
     model_path_name = model_name.replace("/", "__")
 
-    slurm_job_file = slurm_jobs_folder / f"{model_path_name}_{task_type}.sh"
+    slurm_job_file = slurm_jobs_folder / f"{model_path_name}_{task_name}.sh"
     with open(slurm_job_file, "w") as f:
         f.write(slurm_job)
     return slurm_job_file
@@ -51,6 +52,8 @@ def create_slurm_job_files(
     results_folder: Path,
     slurm_prefix: str,
     slurm_jobs_folder: Path,
+    batch_size: int,
+    device: str
 ) -> list[Path]:
     """Create slurm job files for running models on all tasks"""
     slurm_job_files = []
@@ -62,6 +65,8 @@ def create_slurm_job_files(
                 results_folder,
                 slurm_prefix,
                 slurm_jobs_folder,
+                batch_size,
+                device
             )
             slurm_job_files.append(slurm_job_file)
     return slurm_job_files
@@ -90,9 +95,9 @@ if __name__ == "__main__":
 
 
     # project_root = Path(__file__).parent / ".." / ".." / ".."
-    results_folder = Path(__file__).parent / "results"
+    results_folder = Path(__file__) / "results"
     # results_folder = Path("/data/niklas/results")
-    slurm_jobs_folder = Path(__file__).parent / "slurm_jobs"
+    slurm_jobs_folder = Path(__file__) / "slurm_jobs"
 
     tasks = mteb.get_tasks(languages=["eng"],modalities=["text", "image"],
                        task_types=args.task_type)
@@ -103,6 +108,6 @@ if __name__ == "__main__":
     slurm_jobs_folder.mkdir(exist_ok=True)
     results_folder.mkdir(exist_ok=True)
     files = create_slurm_job_files(
-        args.model_names, task_names, results_folder, slurm_prefix, slurm_jobs_folder, args.device
+        args.model_names, task_names, results_folder, slurm_prefix, slurm_jobs_folder, args.batch_size, args.device
     )
     run_slurm_jobs(files)
