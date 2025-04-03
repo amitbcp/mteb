d6f5754eb792f5af725a50d9559e5a8490d34631
Amit Agarwal <amit.h.agarwal@oracle.com>
2025-04-03 08:59:01 -0700

    resolve merge conflict

--- mteb/leaderboard/__init__.py
+++ mteb/leaderboard/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from mteb.leaderboard.app_local import demo
 from mteb.leaderboard.app import get_leaderboard_app
+from mteb.leaderboard.app_local import demo
 
 __all__ = ["get_leaderboard_app"]
